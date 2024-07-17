ce54913db41b548ee08080cb29714dd2fd19d339
Grant <grant.macmillan94@gmail.com>
2024-07-17 02:00:15 -0400

    continued work on homepage (WIP)

--- app/page.js
+++ app/page.js
@@ -1,9 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FaCheck, FaFire, FaTools, FaUtensils, FaSoap, FaGasPump, FaHamburger } from 'react-icons/fa';
+
 
 
 export default function Home() {
+
+  const cardsData = [
+    {
+      id: 1,
+      title: 'Interior Car Detailing',
+      description: 'Meticulous interior car detailing service to keep your car\'s interior spotless and fresh.',
+      imgSrc: '/images/client-pics/interior1.jpg', // Replace with your image URL
+      link: '/our-services'
+    },
+    {
+      id: 2,
+      title: 'Exterior Car Detailing',
+      description: 'Thorough exterior car detailing to restore and protect your car\'s paint and finish.',
+      imgSrc: '/images/client-pics/cleaning1.jpg', // Replace with your image URL
+      link: '/our-services'
+    },
+    {
+      id: 3,
+      title: 'Clay Bar and Premium Wax',
+      description: 'Advanced clay bar treatment and premium wax for a smooth and glossy car exterior.',
+      imgSrc: '/images/client-pics/exterior1.jpg', // Replace with your image URL
+      link: '/our-services'
+    },
+  ];
+
   return (
     <>
 
@@ -11,8 +39,8 @@ export default function Home() {
         <Container >
           <Row>
             <Col md={6} className={styles.overlay}>
-              <h1 className={styles.heading}>Fall in Love with Your Car Again</h1>
-              <h2 className={styles.subHeading}>London’s Top Rated Eco-Friendly Auto Detailing Shop</h2>
+              <h1 className={styles.heading}>We Bring the Shine to You</h1>
+              <h2 className={styles.subHeading}>Exceptional Care Delivered to Your Location!</h2>
               <div className={styles.buttons}>
                 <Button className={styles.bookNow}>Book Now</Button>
                 <Button className={styles.viewServices}>View Services</Button>
@@ -27,19 +55,30 @@ export default function Home() {
 
 
 
-      <div >
-        <Container>
-          <Row>
-            <Col md={6}>
-              <h1 className={styles.heading}>Our Services</h1>
-              <p className={styles.subHeading}>We offer a wide range of services to keep your car looking its best.</p>
-            </Col>
-            <Col md={6}>
-              <Image src="/images/logo.png" alt="Car" width={500} height={500} />
-            </Col>
+      <section className={styles.servicesSection} >
+        <Container className="py-5">
+          <Row className="justify-content-center">
+            {cardsData.map(card => {
+              const IconComponent = card.icon;
+              return (
+                <Col key={card.id} lg={4} md={6} sm={12} className="mb-4">
+                  <Link href={card.link} passHref style={{ textDecoration: 'none' }}>
+                    <div className={styles.hoverCardLinkWrapper}>
+                      <div className={styles.hoverCard} style={{ backgroundImage: `url(${card.imgSrc})` }}>
+                        <div className={styles.hoverCardContent}>
+                          <h2 className={styles.hoverCardTitle}>{card.title}</h2>
+                          <p className={styles.hoverCardDescription}>{card.description}</p>
+                          <p className={styles.hoverCardLink}>Learn More</p>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </Col>
+              );
+            })}
           </Row>
         </Container>
-      </div>
+      </section>
     </>
 
   );
